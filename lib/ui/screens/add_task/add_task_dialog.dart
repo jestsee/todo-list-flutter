32192cecddee7860c3e18a52fb0b0fd8402464b5
Jesica <13519011@std.stei.itb.ac.id>
2023-02-15 22:42:39 +0700

    feat: slicing bottom tool bar

--- lib/ui/screens/add_task/add_task_dialog.dart
+++ lib/ui/screens/add_task/add_task_dialog.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:todo_list/ui/widgets/subtask_checkbox.dart';
+import 'package:todo_list/ui/widgets/badge.dart';
+import 'package:todo_list/ui/widgets/constants.dart';
+import 'package:todo_list/ui/widgets/custom_button.dart';
 import 'package:todo_list/ui/widgets/subtask_list.dart';
 
 class AddTaskDialog extends StatelessWidget {
@@ -9,25 +11,86 @@ class AddTaskDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            TextFormField(
-              maxLines: null,
-              keyboardType: TextInputType.multiline,
-              initialValue: 'ahoi',
-              style: Theme.of(context).textTheme.headline3,
-              decoration: InputDecoration(
-                  hintText: 'Task title',
-                  border: InputBorder.none,
-                  suffix: IconButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                        // TODO are u sure dont want to save?
-                      },
-                      icon: const Icon(Icons.clear_rounded, size: 28))),
-            ),
-            const SubtaskList()
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  TextFormField(
+                    maxLines: null,
+                    keyboardType: TextInputType.multiline,
+                    initialValue: 'ahoi',
+                    style: Theme.of(context).textTheme.headline3,
+                    decoration: InputDecoration(
+                        hintText: 'Task title',
+                        border: InputBorder.none,
+                        suffix: IconButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                              // TODO are u sure dont want to save?
+                            },
+                            icon: const Icon(Icons.clear_rounded, size: 28))),
+                  ),
+                  const SubtaskList(),
+                  // TODO
+                  const SizedBox(height: 8),
+                  const Align(
+                      alignment: Alignment.bottomRight,
+                      child: Text('Last updated by me at 11.06 PM'))
+                ],
+              ),
+              Column(
+                children: [
+                  CustomButton(
+                      full: true, onPressed: () {}, child: const Text('Save')),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  Container(
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        top: BorderSide(width: 0.75, color: Colors.black45),
+                      ), //add it here
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 18),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 12),
+                            child: Chip(
+                              backgroundColor:
+                                  badgeColor[BadgeVariant.moderate],
+                              label: const Text(
+                                'Medium',
+                                style: TextStyle(
+                                    fontSize: 16, color: Colors.white),
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 10),
+                            ),
+                          ),
+                          GestureDetector(
+                              onTap: () {},
+                              child: const Icon(Icons.group, size: 36)),
+                          GestureDetector(
+                              onTap: () {},
+                              child:
+                                  const Icon(Icons.calendar_month, size: 36)),
+                          GestureDetector(
+                              onTap: () {},
+                              child: const Icon(Icons.location_on, size: 36)),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
