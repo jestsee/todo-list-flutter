d42791e5ca368e00183bbee1c857a2940035abaa
Jesica <13519011@std.stei.itb.ac.id>
2023-04-07 20:42:05 +0700

    feat: persist text on search bar

--- lib/ui/screens/all_task/all_task.dart
+++ lib/ui/screens/all_task/all_task.dart
@@ -1,46 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:todo_list/ui/widgets/search_bar.dart';
 import 'package:todo_list/ui/widgets/task_filter.dart';
 import 'package:todo_list/ui/widgets/task_list.dart';
 import 'package:todo_list/ui/widgets/task_sort.dart';
 
-class AllTask extends StatelessWidget {
+class AllTask extends HookWidget {
   const AllTask({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final showFab = useState(false);
     return Scaffold(
-      floatingActionButton: const TaskSort(),
+      floatingActionButton: showFab.value ? const TaskSort() : null,
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      body: NestedScrollView(
-        floatHeaderSlivers: true,
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return [
-            SliverAppBar(
-              title: const Text('Tasks',
-                  style: TextStyle(fontWeight: FontWeight.bold)),
-              centerTitle: true,
-              floating: true,
-              snap: true,
-              bottom: PreferredSize(
-                preferredSize: const Size.fromHeight(80),
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(24, 0, 24, 20),
-                  child: Row(
-                    children: const [
-                      Expanded(child: SearchBar()),
-                      SizedBox(width: 12),
-                      TaskFilter()
-                    ],
+      body: NotificationListener<UserScrollNotification>(
+        onNotification: (notification) {
+          if (notification.direction == ScrollDirection.forward) {
+            if (!showFab.value) showFab.value = true;
+          } else if (notification.direction == ScrollDirection.reverse) {
+            if (showFab.value) showFab.value = false;
+          }
+          return true;
+        },
+        child: NestedScrollView(
+          floatHeaderSlivers: true,
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                title: const Text('Tasks',
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                centerTitle: true,
+                floating: true,
+                snap: true,
+                bottom: PreferredSize(
+                  preferredSize: const Size.fromHeight(80),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(24, 0, 24, 20),
+                    child: Row(
+                      children: const [
+                        Expanded(child: SearchBar()),
+                        SizedBox(width: 12),
+                        TaskFilter()
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            )
-          ];
-        },
-        body: const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
-          child: TaskList(filtered: true),
+              )
+            ];
+          },
+          body: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: TaskList(filtered: true),
+          ),
         ),
       ),
     );
--- lib/ui/widgets/search_bar.dart
+++ lib/ui/widgets/search_bar.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:todo_list/provider.dart';
 
-class SearchBar extends ConsumerWidget {
+class SearchBar extends HookConsumerWidget {
   const SearchBar({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final textController =
+        useTextEditingController(text: ref.read(searchFilterProvider));
     return TextField(
+      controller: textController,
       decoration: InputDecoration(
           filled: true,
           fillColor: Colors.white,
